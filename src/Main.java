f855276066b04298eb7c7d7fc6d723873494b9bf
gamesman36 <gamesman36@outlook.com>
2023-06-11 20:08:45 +0200

    Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        // Construct complete vehicles
         Vehicle minivan = new Vehicle(7, 16, 21);
         Vehicle sportscar = new Vehicle(2, 14, 12);
-        double gallons;
+
         int dist = 252;
 
-        gallons = minivan.fuelneeded(dist);
+        double gallons = minivan.fuelNeeded(dist);
+        System.out.println("To go " + dist + " miles, minivan needs " +
+                gallons + " gallons of fuel.");
 
-        System.out.println("To go " + dist + " miles minivan needs " +
-                           gallons + " gallons of fuel.");
+        gallons = sportscar.fuelNeeded(dist);
+        System.out.println("To go " + dist + " miles, sportscar needs " +
+                gallons + " gallons of fuel.");
 
-        gallons = sportscar.fuelneeded(dist);
+        int minivanRange = minivan.range();
+        System.out.println("The range of minivan is " + minivanRange + " miles.");
 
-        System.out.println("To go " + dist + " miles sportscar needs " +
-                           gallons + " gallons of fuel.");
+        int sportscarRange = sportscar.range();
+        System.out.println("The range of sportscar is " + sportscarRange + " miles.");
+
+        int minivanPassengers = minivan.getPassengers();
+        System.out.println("The number of passengers in minivan is " + minivanPassengers + ".");
+
+        int sportscarFuelCap = sportscar.getFuelCap();
+        System.out.println("The fuel capacity of sportscar is " + sportscarFuelCap + " gallons.");
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,23 +1,31 @@
 class Vehicle {
+    private final int passengers;  // number of passengers
+    private final int fuelcap;     // fuel capacity in gallons
+    private final int mpg;         // fuel consumption in miles per gallon
 
-    int passengers; // number of passengers
-    int fuelcap; // fuel capacity in gallons
-    int mpg; // fuel consumption in miles per gallon
-
-    // This is a constructor for Vehicle
-    Vehicle(int p, int f, int m) {
+    // constructor for Vehicle
+    public Vehicle(int p, int f, int m) {
         passengers = p;
         fuelcap = f;
         mpg = m;
     }
 
-    // Return the range
-    int range() {
+    // accessor methods
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public int getFuelCap() {
+        return fuelcap;
+    }
+
+    // return the range of the vehicle
+    public int range() {
         return mpg * fuelcap;
     }
 
-    // Compute fuel needed for a given distance
-    double fuelneeded(int miles) {
+    // compute fuel needed for a given distance
+    public double fuelNeeded(int miles) {
         return (double) miles / mpg;
     }
 }
